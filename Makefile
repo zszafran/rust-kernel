2391aff1d782150cf9084dceda522da47ba8e1fc
Zach Szafran <zszafran@gmail.com>
2020-08-15 23:39:28 +0000

    Initial minimal kernel.

--- Makefile
+++ Makefile
@@ -1,5 +1,10 @@
-RUST=cargo rustc
-FLAGS=link-arg=-nostartfiles
+default: run
 
-make:
-	$(RUST) -- -C $(FLAGS)
+bootimage:
+	cargo bootimage
+
+run: bootimage
+	qemu-system-x86_64 -drive format=raw,file=target/x86_64-freestanding/debug/bootimage-rust-kernel.bin
+
+run-curses: bootimage
+	qemu-system-x86_64 -curses -drive format=raw,file=target/x86_64-freestanding/debug/bootimage-rust-kernel.bin
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,27 @@
 #![no_main]
 #![allow(dead_code)]
 
+extern crate rlibc;
+
 use core::panic::PanicInfo;
+
+static HELLO: &[u8] = b"Hello World!";
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    loop {}
+  let vga_buffer = 0xb8000 as *mut u8;
+
+  for (i, &byte) in HELLO.iter().enumerate() {
+    unsafe {
+      *vga_buffer.offset(i as isize * 2) = byte;
+      *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+    }
+  }
+
+  loop {}
 }
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+  loop {}
 }
